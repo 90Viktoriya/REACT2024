9b80ee96312f0c8d6c282821814ae84fb04d44cb
90Viktoriya <viktoria.kileeva@gmail.com>
2024-07-15 17:42:53 +0300

    feat: add two tests

--- src/features/DetailedCard/DetailedCard.test.tsx
+++ src/features/DetailedCard/DetailedCard.test.tsx
@@ -1,36 +1,45 @@
-import { describe, expect, vi } from 'vitest';
+import { describe, expect, Mock, vi } from 'vitest';
 import { render } from '@testing-library/react';
 import { DetailedCard } from './DetailedCard';
-import { MemoryRouter, useLoaderData } from 'react-router-dom';
+import { MemoryRouter, useLoaderData, useNavigation } from 'react-router-dom';
 
-beforeEach(() => {
-  vi.clearAllMocks();
-});
-const mockedNavigate = vi.fn();
 vi.mock('react-router-dom', async () => {
   const mod = await vi.importActual('react-router-dom');
   return {
     ...mod,
-    useNavigation: () => ({
-      navigation: mockedNavigate,
-      state: 'idle'
-    }),
+    useNavigation: vi.fn(),
     useLoaderData: vi.fn()
   };
 });
-
+vi.mocked(useLoaderData).mockReturnValue({
+  uid: 'test',
+  name: 'Detailed card test',
+  gender: '',
+  yearOfBirth: 0,
+  placeOfBirth: 'test',
+  yearOfDeath: 0,
+  performers: [{}],
+  episodes: [{}],
+  movies: [{}]
+});
+vi.mock('../../components/Loader', () => ({
+  Loader: () => <div>Loader</div>
+}));
 describe('Given DetailedCard component', () => {
+  it('when navigation state = "loading", loader should be displayed', async () => {
+    (useNavigation as Mock).mockReturnValue({
+      state: 'loading'
+    });
+    const screen = render(
+      <MemoryRouter>
+        <DetailedCard />
+      </MemoryRouter>
+    );
+    expect(screen.findByText('Loader')).toBeTruthy();
+  });
   it('when rendered, should contain relevant data', () => {
-    vi.mocked(useLoaderData).mockReturnValue({
-      uid: 'test',
-      name: 'Detailed card test',
-      gender: '',
-      yearOfBirth: 0,
-      placeOfBirth: 'test',
-      yearOfDeath: 0,
-      performers: [{}],
-      episodes: [{}],
-      movies: [{}]
+    (useNavigation as Mock).mockReturnValue({
+      state: 'idle'
     });
     const screen = render(
       <MemoryRouter>
--- src/pages/Main/Main.test.tsx
+++ src/pages/Main/Main.test.tsx
@@ -1,32 +1,42 @@
-import { describe, expect, vi } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, expect, Mock, vi } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
 import { Main } from './Main';
-beforeEach(() => {
-  vi.clearAllMocks();
+import { useNavigation } from 'react-router-dom';
+vi.mock('react-router-dom', async () => {
+  const mod = await vi.importActual('react-router-dom');
+  return {
+    ...mod,
+    useNavigation: vi.fn(),
+    useNavigate: vi.fn(),
+    useFetcher: vi.fn(),
+    useLocation: () => ({ pathname: '/' })
+  };
 });
+vi.mock('../../features/Search/Search', () => ({
+  Search: () => <div>Search</div>
+}));
+vi.mock('../../features/Result/Result', () => ({
+  Result: () => <div>Result test</div>
+}));
+vi.mock('../../components/Loader', () => ({
+  Loader: () => <div>Loader</div>
+}));
+afterEach(() => cleanup());
 describe('Given Main component', () => {
-  it('when navigation state not "loading", result should be displayed', () => {
-    vi.mock('react-router-dom', async () => {
-      const mod = await vi.importActual('react-router-dom');
-      return {
-        ...mod,
-        useNavigation: () => ({
-          navigation: vi.fn(),
-          state: 'idle'
-        }),
-        useNavigate: vi.fn(),
-        useFetcher: vi.fn(),
-        useLocation: () => ({ pathname: '/' })
-      };
+  it('when navigation state = "loading", loader should be displayed', () => {
+    (useNavigation as Mock).mockReturnValue({
+      state: 'loading'
     });
-    vi.mock('../../features/Search/Search', () => ({
-      Search: () => <div>Search</div>
-    }));
-    vi.mock('../../features/Result/Result', () => ({
-      Result: () => <div>Result</div>
-    }));
     const searchValue = 'Test Value';
     const screen = render(<Main searchValue={searchValue} />);
-    expect(screen.findByText('Result')).toBeTruthy();
+    expect(screen.findByText('Loader')).toBeTruthy();
+  });
+  it('when navigation state not "loading", result should be displayed', () => {
+    (useNavigation as Mock).mockReturnValue({
+      state: 'idle'
+    });
+    const searchValue = 'Test Value';
+    const screen = render(<Main searchValue={searchValue} />);
+    expect(screen.findByText('Result test')).toBeTruthy();
   });
 });
